e926b2ae644006166a9ad404c5c20181a30a01b7
Rene Santana <renesantana10@gmail.com>
2020-02-27 06:28:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,5 +50,5 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else if (cardNumber) {
     cart = []
-return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+return `Your total cost is $${total().totalPrice}, which will be charged to the card ${cardNumber}.`
 } }
