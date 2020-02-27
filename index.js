e53875462d83c3a79a8684fbbbfc8a1b098fb437
Rene Santana <renesantana10@gmail.com>
2020-02-27 05:42:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,16 +23,15 @@ function viewCart() {
   } else if (cart.length === 1) {
     return `${cartString} ${cart[0].itemName} at $${cart[0].itemPrice}.`
   } else if (cart.length === 2); {
-    return `${cartString} ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
-  } else {
-    
-  }
+    return `${cartString} ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`} else  { for (var i = 0; i < cart.length; i++){
+    }
+    }
 }
 
 
 function total() {
   var totalPrice = 0;
-  for (let i = 0; i < cart.length; i++){
+  for (var i = 0; i < cart.length; i++){
     totalPrice = totalPrice + cart[i].itemPrice;
   }
 return totalPrice
