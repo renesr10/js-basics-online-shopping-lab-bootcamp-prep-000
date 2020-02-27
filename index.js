520fcfecfbc1049e9c40d7937970b0ff554ad19e
Rene Santana <renesantana10@gmail.com>
2020-02-27 06:47:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,10 +46,10 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === undefined){
-    return "Sorry, we don't have a credit card on file for you."
-  } else if (cardNumber) {
+   if (cardNumber) {
     cart = []
 return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-} }
+}  else {
+    return "Sorry, we don't have a credit card on file for you."
+  }}
 
