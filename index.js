79d756576084a556b25a12c2c47d38044ad1923f
Rene Santana <renesantana10@gmail.com>
2020-02-27 06:49:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function viewCart() {
 }
 function total() {
   var totalPrice = 0;
-  for (var i = 0; i < cart.length; i++){
+  for (let i = 0; i < cart.length; i++){
     totalPrice += cart[i].itemPrice;
   }
   return totalPrice
